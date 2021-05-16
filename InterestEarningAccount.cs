dedf4caa52b87a3b41253ff93fee5d0b6fb1a94a
Michal Siedlecki <siedlecki.michal@gmail.com>
2021-05-16 10:49:04 +0200

    Initial commit

---
+++ InterestEarningAccount.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BankApplication
+{
+    class InterestEarningAccount : BankAccount
+    {
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace BankApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var account = new BankAccount("michal", 1000);
+            Console.WriteLine($"Account {account.Number} was created for {account.Owners} with {account.Balance} initial balance.");
+        
+            var account2 = new BankAccount("michal", 1000);
+            Console.WriteLine($"Account {account2.Number} was created for {account2.Owners} with {account2.Balance} initial balance.");
+
+            account.MakeWithdrawal(500, DateTime.Now, "Rent payment");
+            Console.WriteLine(account.Balance);
+            account.MakeDeposit(100, DateTime.Now, "Friend paid me back");
+            Console.WriteLine(account.Balance);
+
+            try
+            {
+                var account3 = new BankAccount("john", -55);
+            }
+            catch (ArgumentOutOfRangeException e)
+            {
+                Console.WriteLine("Exception caught creating account with negative balance");
+                Console.WriteLine(e.ToString());
+            }
+
+            Console.WriteLine(account2.GetAccountHistory());
+        }
+    }
+}
